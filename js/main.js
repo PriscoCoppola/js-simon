91b663e302d77dff40de14e35e8dfb5d430b17d3
PriscoCoppola <priscocoppola7@gmail.com>
2021-04-12 14:55:29 +0200

    finish

--- js/main.js
+++ js/main.js
@@ -10,14 +10,60 @@
 // Alert 5 numeri casuali
 
 var numeriRandom = [];
+var counter = 0;
+var size = 5;
 
-for (var i = 0; i < 5; i++) {
+while (counter < size) {
     var numero = Math.floor(Math.random() * 100) + 1;
     numeriRandom.push(numero);
+
+    counter++;
 }
 
 alert(numeriRandom);
 
 // Timer di 30 secondi
+var secondi = 30;
+
+var id = setInterval(function() {
+    if (secondi === 0) {
+        clearInterval(id);
+        
+        // Chiedere all'utente di inserire i numeri visti precedentemente
+        var numeriUser = [];
+        
+        for (var i = 1; i <= size; i++) {
+            var user = parseInt(prompt('Inserisci numero ' + i + ' di ' + size))
+            numeriUser.push(user);
+        }
+        
+        // Numeri indovinati
+        var numeriIndovinati = [];
+
+        for (var i = 0; i < numeriUser.length; i++) {
+            if (numeriRandom.includes(numeriUser[i])) {
+                numeriIndovinati.push(numeriUser[i]);
+            }
+        }
+
+        // Numeri sbagliati
+        var numeriSbagliati = [];
+        
+        for (var i = 0; i < numeriUser.length; i++) {
+            if (! numeriRandom.includes(numeriUser[i])) {
+                numeriSbagliati.push(numeriUser[i]);
+            }
+        }
+        
+        // Stampa risultato
+        console.log('Hai indovinato ' + numeriIndovinati.length + ' numeri su ' + size + '. ' + 'Cioè ' + numeriIndovinati);
+        console.log('Non hai indovinato ' + numeriSbagliati.length + ' numeri su ' + size + '. ' + 'Cioè ' + numeriSbagliati);
+
+    } else {
+        secondi--;
+    }
+}, 1000)
+
+
 
 
